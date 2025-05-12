cad4ef35f4dce286d79861398bcf1fe7e01eb4b4
SaiedGaffer <saied2421998@gmail.com>
2025-05-12 13:57:42 +0300

    add pg_trgm index to product name

---
+++ product/migrations/0003_add_trgm_to_product_name.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('product', '0002_enable_pg_trgm'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                CREATE INDEX product_name_trgm_idx
+                ON product_product
+                USING GIN (name gin_trgm_ops);
+            """,
+            reverse_sql="DROP INDEX product_name_trgm_idx;",
+        ),
+    ]
