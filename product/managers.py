c2267c698d6cde264dd89c9763c54b0af9cec8d0
SaiedGaffer <saied2421998@gmail.com>
2025-05-13 15:17:35 +0300

    combine missplings and partial keyword search

--- product/managers.py
+++ product/managers.py
@@ -2,15 +2,24 @@
 
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db import models
+from django.db.models import Q, Value
+from django.db.models.functions import Lower
 
 
 class ProductQuerySet(models.QuerySet):
-    def fuzzy(self, term, threshold=0.13):
+    def smart_search(self, term, sim_threshold=0.1):
+        term_lower = term.lower()
         return (
             self
-            .annotate(similarity=TrigramSimilarity('name', term))
-            .filter(similarity__gt=threshold)
-            .order_by('-similarity')
+            .annotate(
+                name_lower=Lower('name'),
+                sim=TrigramSimilarity(Lower('name'), Value(term_lower))
+            )
+            .filter(
+                Q(name_lower__icontains=term_lower)
+                | Q(sim__gt=sim_threshold)
+            )
+            .order_by('-sim')
         )
 
 
@@ -18,5 +27,5 @@ class ProductManager(models.Manager):
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self._db)
 
-    def fuzzy_search(self, term, **kwargs):
-        return self.get_queryset().fuzzy(term, **kwargs)
+    def smart_search(self, term, **kwargs):
+        return self.get_queryset().smart_search(term, **kwargs)
