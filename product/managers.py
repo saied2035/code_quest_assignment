94c169b19822c4ad9ec4a705f451cb334b55c8f3
SaiedGaffer <saied2421998@gmail.com>
2025-05-13 15:23:54 +0300

    update smart search to include full text search

--- product/managers.py
+++ product/managers.py
@@ -1,27 +1,30 @@
-# products/managers.py
+# product/managers.py
 
-from django.contrib.postgres.search import TrigramSimilarity
 from django.db import models
-from django.db.models import Q, Value
+from django.db.models import Q, Value, F
 from django.db.models.functions import Lower
-
+from django.contrib.postgres.search import (
+    SearchQuery, SearchRank, TrigramSimilarity
+)
 
 class ProductQuerySet(models.QuerySet):
-    def smart_search(self, term, sim_threshold=0.1):
+    def smart_search(self, term, sim_threshold=0.13):
         term_lower = term.lower()
+        query = SearchQuery(term_lower, config='simple')  # supports Arabic + English
+
         return (
-            self
-            .annotate(
+            self.annotate(
                 name_lower=Lower('name'),
-                sim=TrigramSimilarity(Lower('name'), Value(term_lower))
+                sim=TrigramSimilarity(Lower('name'), Value(term_lower)),
+                rank=SearchRank(F('search_vector'), query)
             )
             .filter(
-                Q(name_lower__icontains=term_lower)
-                | Q(sim__gt=sim_threshold)
+                Q(name_lower__icontains=term_lower) |
+                Q(sim__gt=sim_threshold) |
+                Q(search_vector=query)
             )
-            .order_by('-sim')
+            .order_by('-rank', '-sim')
         )
-
 
 class ProductManager(models.Manager):
     def get_queryset(self):
