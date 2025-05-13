8ca892a7410f8941b465338594f7e26f30f2716f
SaiedGaffer <saied2421998@gmail.com>
2025-05-13 15:58:24 +0300

    modify command code for better update for search vector

--- product/management/commands/update_search_vectors.py
+++ product/management/commands/update_search_vectors.py
@@ -4,12 +4,20 @@ from django.core.management.base import BaseCommand
 from django.contrib.postgres.search import SearchVector
 from product.models import Product
 
+
 class Command(BaseCommand):
     help = "Bulk update search_vector field for all products"
 
     def handle(self, *args, **kwargs):
-        Product.objects.update(
-            search_vector=SearchVector('name', config='english') +
-                          SearchVector('name', config='arabic')
+        products = Product.objects.annotate(
+            search_vector_combined=(
+                SearchVector('name', config='english') +
+                SearchVector('name', config='arabic')
+            )
         )
+
+        for product in products:
+            Product.objects.filter(pk=product.pk).update(
+                search_vector=product.search_vector_combined)
+
         self.stdout.write(self.style.SUCCESS("Updated all search vectors"))
