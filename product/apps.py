766e2415544825c7cd536185b91903c216aff619
SaiedGaffer <saied2421998@gmail.com>
2025-05-13 15:57:18 +0300

    dynamic import for product.signals

--- product/apps.py
+++ product/apps.py
@@ -1,9 +1,9 @@
+import importlib
 from django.apps import AppConfig
-
 
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
 
     def ready(self):
-        import product.signals
+        importlib.import_module('product.signals')
