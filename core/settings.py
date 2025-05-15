449414e9c88956b58b2a9d297aa8590b439ca2d9
SaiedGaffer <saied2421998@gmail.com>
2025-05-15 13:56:27 +0300

    add default configration for drf

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,22 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
+REST_FRAMEWORK = {
+    # 1) Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    # 2) Rate-Limiting / Throttling
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',   # anonymous users: 20 requests per minute
+        'user': '100/minute',  # authenticated users: 100/minute
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
